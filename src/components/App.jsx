4325d2a5da783a2ab5e832c9934a62a7ee01c089
Elizabeth Cheng <echeng924@gmail.com>
2016-12-27 21:24:29 -0500

    add script for google maps api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
+
 
 class App extends Component {
   render() {
@@ -6,6 +8,34 @@ class App extends Component {
       <div>
         Hello world.
       </div>
+      <section style={{height: "100%"}}>
+      <GoogleMapLoader
+        containerElement={
+          <div
+            {...props.containerElementProps}
+            style={{
+              height: "100%",
+            }}
+          />
+        }
+        googleMapElement={
+          <GoogleMap
+            ref={(map) => console.log(map)}
+            defaultZoom={3}
+            defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+            onClick={props.onMapClick}
+          >
+            {props.markers.map((marker, index) => {
+              return (
+                <Marker
+                  {...marker}
+                  onRightclick={() => props.onMarkerRightclick(index)} />
+              );
+            })}
+          </GoogleMap>
+        }
+      />
+    </section>
     );
   };
 }
